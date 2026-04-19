2a8ef1880cb08a9184729e6f1a9c6c53739ec2cc
roman-yakubovskyi <yakubovskyi133@gmail.com>
2026-04-20 01:43:46 +0200

    add fix type

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,5 +1,6 @@
 import styled from './Container.module.css';
+import { ReactNode } from 'react';
 
-export default function Container({ children }) {
+export default function Container({ children }: { children: ReactNode }) {
   return <div className={styled.container}>{children}</div>;
 }
